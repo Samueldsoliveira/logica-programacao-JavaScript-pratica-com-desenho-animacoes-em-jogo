4980a5309d358eede8eab3f66489d693699b7ec3
samueldsoliveira <samuelzinS10@gmail.com>
2023-02-10 15:37:08 -0300

    circle-added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var tela = document.querySelector('canvas');
-var pincel = tela.getContext('2d');
+var pincel = tela.getContext('2d'); //Uma API para desenhos geométricos
 
 pincel.fillStyle = 'lightgrey'; //Seleciona a cor do pincel
 pincel.fillRect(0, 0, 600, 400); //Seleciona o caminho em que o pincel irá preencher
@@ -16,4 +16,9 @@ pincel.moveTo(300,200); //moveTo = "Mova-se até"
 pincel.lineTo(200, 400);
 //        eixo X || Y
 pincel.lineTo(400, 400);
+pincel.fill();
+
+pincel.fillStyle = 'blue';
+pincel.beginPath();
+pincel.arc(300, 200, 50, 0, 2 * 3.14); //Cria um circulo
 pincel.fill();
