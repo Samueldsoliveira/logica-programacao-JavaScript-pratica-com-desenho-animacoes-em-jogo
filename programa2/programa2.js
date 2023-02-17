403c1b01b58be28d2ce4949a208ca36b587898bb
samueldsoliveira <samuelzinS10@gmail.com>
2023-02-17 16:27:47 -0300

    functions-and-repeating-structure

--- programa2/programa2.js
+++ programa2/programa2.js
@@ -9,11 +9,23 @@ function desenhaQuadrado(x, y, cor){
     pincel.strokeRect(x, y, 50, 50); //Faz um contorno
     
 }
+/*
+var x = 0;
+var y = 0;
+while(x < 600 && y < 600){
+    desenhaQuadrado( x, y, 'red');
+    desenhaQuadrado( x, y, 'blue');
+    desenhaQuadrado( x, y, 'white');
+    x += 50;
+    y += 0;
+}
+*/
+
+for(var x = 0; x < 600; x + 50){
+    desenhaQuadrado( x, 0, 'red');
+}
 
 
-desenhaQuadrado(0, 0, 'green');
-desenhaQuadrado(50, 0, 'green');
-desenhaQuadrado(100, 0, 'green');
 
 
 
